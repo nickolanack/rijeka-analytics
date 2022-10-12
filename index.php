15e327e16eae28c53465b06fd492cef19d2bf7d6
Nick Blackwell <nickblackwell82@gmail.com>
2022-10-11 20:43:42 -0700

    add intial page, libs

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Analytics</title>
+		<script type="text/javascript">
+			if(!window.console){
+			window.console={
+				"log":function(){},
+				"error":function(){},
+				"warn":function(){},
+				"info":function(){}
+			}
+		}
+		</script>
+		<script type="text/javascript" src="https://aopfn.geoforms.ca/app/nickolanack/php-core-app/assets/js/ClassObject.js?1655843160"></script>
+		<script type="text/javascript" src="https://aopfn.geoforms.ca/app/nickolanack/php-core-app/assets/js/Window.js?1655851684"></script>
+		<script type="text/javascript" src="https://d26b395fwzu5fz.cloudfront.net/keen-analysis-1.2.2.js"></script>
+		<script type="text/javascript" src="https://d26b395fwzu5fz.cloudfront.net/keen-dataviz-1.1.3.js"></script>
+		<script type="text/javascript" src="https://d26b395fwzu5fz.cloudfront.net/keen-dataviz-1.1.3.css"></script>
+		<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+		<script type="text/javascript">
+			google.charts.load('current', {
+				'packages':['geochart'],
+			});
+			
+			google.charts.setOnLoadCallback(drawRegionsMap);
+			function drawRegionsMap() {
+				var data = google.visualization.arrayToDataTable([
+					['Country', 'Popularity'],
+					['Germany', 200],
+					['United States', 300],
+					['Brazil', 400],
+					['Canada', 500],
+					['France', 600],
+					['RU', 700]
+					]);
+				var options = {};
+				var chart = new google.visualization.GeoChart(document.getElementById('regions_div'));
+				chart.draw(data, options);
+			}
+		</script>
+	</head>
+	<body>
+		<div id="regions_div" style="width: 900px; height: 500px;"></div>
+	</body>
+	
+</body>
+</html>
