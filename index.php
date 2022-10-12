99406674e6173e393c081d3f375e9dbabc724f60
Nick Blackwell <nickblackwell82@gmail.com>
2022-10-12 12:13:12 -0700

    adjust colors

--- index.php
+++ index.php
@@ -133,7 +133,7 @@ foreach($ipmap as $ip=>$country){
 
 				}, array_keys($countries)), JSON_PRETTY_PRINT); ?>));
 				var options = {
-					colorAxis: {colors: ['#cccccc', 'rgb(0, 187, 222)']},
+					colorAxis: {colors: ['rgb(161 199 211)', 'rgb(0, 187, 222)']},
 					region:150
 				};
 				var chart = new google.visualization.GeoChart(document.getElementById('regions_div'));
