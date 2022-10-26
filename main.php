0032e5e09280e9ab7afd3e4776fc7ccac47405a0
Nick Blackwell <nickblackwell82@gmail.com>
2022-10-26 12:42:22 -0700

    add class names and css placeholder

--- main.php
+++ main.php
@@ -2,7 +2,7 @@
 		<main>
 			<h1>Analytics. <span>Rijeka in Flux</span> mobile app</h1>
 			<section>
-				<p style="text-align: right;"><button class="region" onclick="showRegion();">Region</button><button class="world" onclick="showWorld();">World</button></p>
+				<p style="text-align: right;"><button class="region active" onclick="showRegion();">Region</button><button class="world" onclick="showWorld();">World</button></p>
 				<div id="regions_div" style="">
 					
 				</div>
