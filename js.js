b42002bd37c1557445cec2ad9781124c384417c9
Nick Blackwell <nickblackwell82@gmail.com>
2022-10-26 12:25:44 -0700

    move drawCloropleth to js

--- js.js
+++ js.js
@@ -1,9 +1,60 @@
 'use strict'
 
-(function(){
+var drawCloroplethMap = (function(){
 
 
-	console.log('hello world');
+	google.charts.load('current', {
+		'packages':['geochart'],
+	});
 
+
+
+	var loaded=false;
+	function onLoadCharts() {
+		loaded=true;
+		if(buff.length){
+			var b=buff.slice(0);
+			buff=[];
+			b.forEach(function(f){ f(); };
+		}
+	}
+			
+	
+
+	
+
+
+
+	var drawCloroplethMap=function(region, chartData, divId){
+
+		var render=function(){
+
+			var data = google.visualization.arrayToDataTable(([['Country', 'Unique IPs']]).concat(chartData));
+			var options = {
+				colorAxis: {colors: ['rgb(161, 199, 211)', 'rgb(0, 187, 222)', 'rgb(254, 102, 114)']},
+				region:region
+			};
+			var chart = new google.visualization.GeoChart(document.getElementById(divId));
+			chart.draw(data, options);
+
+		};
+
+
+		if(laoded){
+			render();
+			return;
+		}
+
+		buff.push(render);
+
+
+	};
+
+	google.charts.load('current', {
+		'packages':['geochart'],
+	});
+	google.charts.setOnLoadCallback(onLoadCharts);
+
+	return drawCloroplethMap;
 
 })();
