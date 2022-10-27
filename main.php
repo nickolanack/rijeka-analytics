c61d2c32ac9e3218be22bfc2979efdc8a83dad2f
Nick Blackwell <nickblackwell82@gmail.com>
2022-10-26 22:56:23 -0700

    wrong el

--- main.php
+++ main.php
@@ -55,8 +55,8 @@
 				<h2>Unique section views</h2>
 				<p>*These are not the only sections that are logged as `section views`, but are easily distinguished from others. Because some section views do not request unique data, they cannot be identified as a specific app section and are not included below.</p>
 				
-				<p style="text-align: right;"><button class="tours active" onclick="showMetrics('tours');">Tours</button><button class="categories" onclick="showMetrics('categories');">Categories</button><button class="researchers" onclick="showMetrics('researchers');">Researchers</button></p>
-				<p class="toggle-btns" style="text-align: right;"><label class="unique-u">Unique users</label> <label class="total-u" >Total views</label></p>
+				<p class="toggle-btns"  style="text-align: right;"><button class="tours active" onclick="showMetrics('tours');">Tours</button><button class="categories" onclick="showMetrics('categories');">Categories</button><button class="researchers" onclick="showMetrics('researchers');">Researchers</button></p>
+				<p style="text-align: right;"><label class="unique-u">Unique users</label> <label class="total-u" >Total views</label></p>
 				<div class="metric-section with-overlay tours">
 				
 					<div id="metrics_tours_div_unique" class=" metrics-group">	
